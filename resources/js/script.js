093144afd1989e606a7c46719c34446f6d892a37
msalam <ms17alam@gmail.com>
2018-03-13 14:33:49 -0400

    working on jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
     
-    $("h1").click(function(){
-        $(this).css("background-color", "#ff0000")
-    })
+/* ----STICKY NAV-----*/
+    
+    $(".js--section-features").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    }, {
+        offset: "60px;"
+    });
     
     
+    /*-----SCROLL ON BUTTON-----*/
+    
+    
+    $(".js--scroll-to-plans").click(function(){
+        $("html, body").animate({scrollTop: $(".js--section-plans").offset().top}, 1000);
+    });
+    
+    
+     $(".js--scroll-to-start").click(function(){
+        $("html, body").animate({scrollTop: $(".js--section-features").offset().top}, 1000);
+    });
+    
+    
+    /*-----NAVIGATION SCROLL----*/
+    
+        
+
+
 });
+
